0634c3f262fb0be02b4e2953be66e3937df880a9
m246306 <m246306@usna.edu>
2023-02-19 21:30:31 -0500

    Initial commit

---
+++ app/src/main/java/edu/usna/mobileos/a1_tablersamuel/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.usna.mobileos.a1_tablersamuel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
